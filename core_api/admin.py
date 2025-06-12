5c90db211eba52866fa9ae10100bcb432ccb86fe
josearandrade <josearandrade.work@gmail.com>
2025-06-12 10:26:04 -0300

    feat: Create initial models and register in admin panel
    Add HealthProfessional and MedicalAppointment models. Register both in admin panel

--- core_api/admin.py
+++ core_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import HealthProfessional, MedicalAppointment
 
 # Register your models here.
+
+admin.site.register(HealthProfessional)
+admin.site.register(MedicalAppointment)
--- core_api/models.py
+++ core_api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class HealthProfessional(models.Model):
+    social_name = models.CharField(max_length=255)
+    profession = models.CharField(unique=True)
+    # Address information
+    address_street = models.CharField(max_length=100, blank=True, null=True)
+    address_number = models.CharField(max_length=10, blank=True, null=True)
+    address_city = models.CharField(max_length=50, blank=True, null=True)
+    address_state = models.CharField(max_length=30, blank=True, null=True)
+    address_zip = models.CharField(max_length=15, blank=True, null=True)
+    # Contact information
+    contact_email = models.EmailField(max_length=100, blank=True, null=True)
+    contact_phone = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.social_name
+    
+class MedicalAppointment(models.Model):
+    health_professional = models.ForeignKey(HealthProfessional, on_delete=models.CASCADE, related_name='appointments')
+    appointment_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.health_professional} - {self.appointment_date.strftime('%Y-%m-%d %H:%M')}"
